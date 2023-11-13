f0ff182f7b0b9f997a63dd49408bcc35dfe1b7bc
uniS2 <s2oie1010@gmail.com>
2023-11-14 01:18:56 +0900

    Fix: (bug fix) useSearchParams 인스턴스 반환 버그 해결하기 - 배열 분해하기 - `get` 메서드 통해 원하는 "from" 매개변수 가져오기

--- react-router-tutorial/src/pages/Greeting.jsx
+++ react-router-tutorial/src/pages/Greeting.jsx
@@ -5,7 +5,18 @@ import {
 
 export default function Greeting() {
   const { name } = useParams();
-  const [from] = useSearchParams();
+  //! const [from] = useSearchParams();
+
+  //^ useSearchParams 반환하는 값 = `URLSearchParams`의 인스턴스
+  // URLSearchParams: URL의 쿼리 문자열을 다루기 위한 객체. 검색 매개변수들의 모음으로 객체도 가능하다.
+  // 설정한 queryParamsInput -> queryParams 반환 (Home.jsx)
+
+  //$ 1. 배열("from=queryParamsInput")을 반환하므로 배열을 분해한다.
+  const [searchParams] =
+    useSearchParams(); // 인스턴스 할당
+
+  //$ 2. get 메서드를 사용하여 원하는 특정 매개변수인 "from"의 값을 가져온다.
+  const from = searchParams.get("from");
 
   return (
     <div>
--- react-router-tutorial/src/pages/Home.jsx
+++ react-router-tutorial/src/pages/Home.jsx
@@ -7,10 +7,15 @@ import {
 export default function Home() {
   const [params, setParams] =
     useState("");
+
   const [
     queryParamsInput,
     setQueryParamsInput,
   ] = useState("");
+
+  //^ useSearchParams: 현재 위치에 대한 URL의 쿼리 문자열을 읽고 수정.
+  //^ only for the search portion of the URL
+  //^ `setSearchParams` 지원: 기능 업데이트 -> 반환 함수 `searchParams`
   const [queryParams, setQueryParams] =
     useSearchParams();
 
@@ -18,6 +23,7 @@ export default function Home() {
   //# 2 Navigate
   const navigate = useNavigate();
 
+  // onChange 이벤트
   const handleParamsChange = (e) => {
     setParams(e.target.value);
   };
@@ -26,8 +32,14 @@ export default function Home() {
   ) => {
     setQueryParamsInput(e.target.value);
   };
+
+  // button - onClick 이벤트
   const handleClick = () => {
+    // useState로 설정한 params 변수로 이동
+    // index.js에서 설정한 "/greet/:name" 에 해당
     navigate(`/greet/${params}`);
+
+    //^ 검색 매개변수 - 객체 설정
     setQueryParams({
       from: queryParamsInput,
     });
