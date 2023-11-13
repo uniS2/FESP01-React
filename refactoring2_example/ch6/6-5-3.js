01cb4750bb6060d393b51be7072534ef2f479db1
uniS2 <s2oie1010@gmail.com>
2023-11-13 18:02:26 +0900

    Delete: 리팩토링 학습 공간 분리

--- refactoring2_example/ch6/6-5-3.js
+++
@@ -1,8 +0,0 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
-}
-
-// 함수 선언 바꾸기 (책 6.5)
-// 상황 : 고객의 주(state)가 뉴잉글랜드에 속하는지 확인하는 함수를 만들었습니다.
-// 기대 : 함수가 불필요한 인자를 받고 있지 않나요?
-// 문제파악 : 이 코드는 어떤 문제가 있나요? 없을수도 있어요. 여러분의 생각을 써보세요
